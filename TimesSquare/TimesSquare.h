f4c364d4b8c0becad91c5bb3ce62b87c8786568a
Kra Larivain <olarivain@gmail.com>
2015-06-09 16:55:37 -0700

    -added timesquare dependency

--- TimesSquare/TimesSquare.h
+++ TimesSquare/TimesSquare.h
@@ -7,6 +7,16 @@
 //  See the LICENSE file distributed with this work for the terms under
 //  which Square, Inc. licenses this file to you.
 
+#import <UIKit/UIKit.h>
+
+//! Project version number for TimesSquare.
+FOUNDATION_EXPORT double TimesSquareVersionNumber;
+
+//! Project version string for TimesSquare.
+FOUNDATION_EXPORT const unsigned char TimesSquareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TimesSquare/PublicHeader.h>
+
 #import <TimesSquare/TSQCalendarMonthHeaderCell.h>
 #import <TimesSquare/TSQCalendarRowCell.h>
 #import <TimesSquare/TSQCalendarView.h>

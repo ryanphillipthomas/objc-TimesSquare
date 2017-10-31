e1fc6c5cc822b494e1282f78a6e0bcf8e782808a
Ryan Phillip Thomas <ryanphillipthomas@mac.com>
2017-10-31 08:54:01 -0400

    Update text color

--- TimesSquare/TSQCalendarCell.m
+++ TimesSquare/TSQCalendarCell.m
@@ -41,7 +41,7 @@
     });
     self.shadowOffset = shadowOffset;
     self.columnSpacing = onePixel;
-    self.textColor = [UIColor colorWithRed:0.47f green:0.5f blue:0.53f alpha:1.0f];
+    self.textColor = [UIColor colorWithRed:1.000 green:1.000 blue:1.000 alpha:0.20];
 
     return self;
 }

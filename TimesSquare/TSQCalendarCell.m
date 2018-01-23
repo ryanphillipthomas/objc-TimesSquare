bb08de43f48ee2b62c2f990b5aa98721a40537c2
Ryan Phillip Thomas <ryanphillipthomas@mac.com>
2018-01-22 23:15:40 -0500

    update

--- TimesSquare/TSQCalendarCell.m
+++ TimesSquare/TSQCalendarCell.m
@@ -32,8 +32,8 @@
     self.layoutDirection = [NSLocale characterDirectionForLanguage:languageCode];
 //    self.backgroundColor = [UIColor colorWithRed:0.84f green:0.85f blue:0.86f alpha:1.0f];
     
-    CGFloat onePixel = 1.0f / [UIScreen mainScreen].scale;
-    
+    CGFloat onePixel = 12.0f;
+
     static CGSize shadowOffset;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
